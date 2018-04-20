ae328d04a6a4f6ba27e1ff8c8a1f509d508be19a
Bushra Taimoor <bushra376@hotmail.com>
2018-04-19 22:32:39 -0400

    working express app

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // Making Our Own Twitter
 
 const express = require('express');
-const app = new express();
+const morgan = require('morgan');
 
-app.listen('3000', (req, res, next) => console.log("server running!!"));
+const app = new express();
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//     console.log(req.method, req.path);
+//     next();
+// })
 
 app.get('/', (req, res, next) => {
     res.send("welcome to My Web Page!!");
-})
+});
+
+app.get('/news', (req, res, next) => {
+    res.send("welcome to News Page!! HEADLINES");
+});
+
+app.listen('3000', (req, res, next) => console.log("server running!!"));
