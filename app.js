770c1def8b1a248d2267f7a6ededfc18c977bd4a
Bushra Taimoor <bushra376@hotmail.com>
2018-04-19 23:25:37 -0400

    completed nunjucks integration

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const nunjucks = require("nunjucks");
 
 const app = new express();
 app.use(morgan('dev'));
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
 
 // app.use((req, res, next) => {
 //     console.log(req.method, req.path);
@@ -12,11 +22,20 @@ app.use(morgan('dev'));
 // })
 
 app.get('/', (req, res, next) => {
-    res.send("welcome to My Web Page!!");
+    // res.send("welcome to My Web Page!!");
+    res.render("index", locals);
 });
 
 app.get('/news', (req, res, next) => {
     res.send("welcome to News Page!! HEADLINES");
+});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
 });
 
 app.listen('3000', (req, res, next) => console.log("server running!!"));
